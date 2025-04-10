49858299f61fd112dc60240d4637e3583b0e8c27
abhishek-yadav-2006 <ydvabhishek447@gmail.com>
2025-04-10 11:13:42 +0530

    database and schema updated

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const path = require('path')
-// const mongo_url = mongodb+srv://abhibcs44:zJkGjiw1R3IzTgLT@cluster0.e1tyvzx.mongodb.net/
-// const MONGO_URL = process.env.mongo_url
+const { url } = require('inspector')
 
-// async function main(){
-//     await mongoose.connect()
-// }
-
-
-if(process.env.NODE_ENV != "production"){
-    require("dotenv").config()
+const mongo_url = "mongodb+srv://abhibcs44:zJkGjiw1R3IzTgLT@cluster0.e1tyvzx.mongodb.net/ThanksPal?retryWrites=true&w=majority";
+async function main(){
+    await mongoose.connect(mongo_url)
 }
 
+main().then((res)=>{
+    console.log("connected to db")
+}).catch((err)=>{
+console.log(err)
+})
+// ("dotenv").config()
+
+// const MONGO_URL = process.env.mongo_url
 const app = express()
 
 app.set("view engine", "ejs")
