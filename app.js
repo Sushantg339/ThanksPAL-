d6fba6ec1e257555e8fbda9ec8240e0a1393bb3e
abhishek-yadav-2006 <ydvabhishek447@gmail.com>
2025-04-10 10:30:27 +0530

    /route created

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const path = require('path')
+// const mongo_url = mongodb+srv://abhibcs44:zJkGjiw1R3IzTgLT@cluster0.e1tyvzx.mongodb.net/
+// const MONGO_URL = process.env.mongo_url
+
+// async function main(){
+//     await mongoose.connect()
+// }
+
+
+if(process.env.NODE_ENV != "production"){
+    require("dotenv").config()
+}
+
+const app = express()
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+app.use(express.urlencoded({ extended: true }));
+// app.use(methodOverride("_method"))
+// app.engine("ejs", ejsMate)
+
+app.get("/", (req,res)=>{
+    res.render("index");
+})
+
+
+app.listen(3000, (req,res)=>{
+    console.log("app is listening on port 3000")
+})
---
+++ readme.md
@@ -0,0 +1,2 @@
+atlas : password:   zJkGjiw1R3IzTgLT
+connections string:: mongodb+srv://abhibcs44:zJkGjiw1R3IzTgLT@cluster0.e1tyvzx.mongodb.net/
